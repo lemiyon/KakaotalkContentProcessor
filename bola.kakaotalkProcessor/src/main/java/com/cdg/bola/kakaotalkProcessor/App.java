b080ed26feb72afc632bf234252f0609ae162739
김보라 <alienqueenno.4@gmail.com>
2015-07-22 14:39:11 +0900

    Make and initialize the project

---
+++ bola.kakaotalkProcessor/src/main/java/com/cdg/bola/kakaotalkProcessor/App.java
@@ -0,0 +1,13 @@
+package com.cdg.bola.kakaotalkProcessor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
